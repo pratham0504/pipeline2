401a732854a229f8b875f2c06aea59c511af3223
pratham0504 <pratham.16932@sakec.ac.in>
2024-08-23 16:28:08 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -19,7 +19,7 @@ pipeline {
         }
         stage('SonarQube analysis') {
             steps {
-                withSonarQubeEnv(SonarQubeserver') {
+                withSonarQubeEnv('SonarQubeserver') {
                     bat script: """
                     sonar-scanner -D"sonar.projectKey=python" \
                     -D"sonar.sources=." \
