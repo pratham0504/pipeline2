b655d69ae6c883fdfcb8f5b2a148d0137aa971dd
pratham0504 <pratham.16932@sakec.ac.in>
2024-08-23 16:21:16 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,37 +1,33 @@
 pipeline {
     agent any
     stages {
-       stage('Building') {
-         steps {
-           echo 'Building Stage Running...'
-           echo "Running ${env.BUILD_ID} ${env.BUILD_DISPLAY_NAME} on ${env.NODE_NAME} and JOB ${env.JOB_NAME}"
-         }
-       }
-stage('Testing') {
-         steps {
-           echo 'Testing Stage Running...'
-         }
-       }
-       stage('Deploying') {
-         steps {
-           echo 'Deploying Stage Running...'
-         }
-       }
-   }
+        stage('Building') {
+            steps {
+                echo 'Building Stage Running...'
+                echo "Running ${env.BUILD_ID} ${env.BUILD_DISPLAY_NAME} on ${env.NODE_NAME} and JOB ${env.JOB_NAME}"
+            }
+        }
+        stage('Testing') {
+            steps {
+                echo 'Testing Stage Running...'
+            }
+        }
+        stage('Deploying') {
+            steps {
+                echo 'Deploying Stage Running...'
+            }
+        }
+        stage('SonarQube analysis') {
+            steps {
+                withSonarQubeEnv('SonarQubeserver') {
+                    bat script: """
+                    sonar-scanner -D"sonar.projectKey=python" \
+                    -D"sonar.sources=." \
+                    -D"sonar.host.url=http://localhost:9000" \
+                    -D"sonar.login=sqp_c84a56847a2f17688bfe80a1d533feac2acab633"
+                    """
+                }
+            }
+        }
+    }
 }
-stage('SonarQube analysis') {
-steps {
-withSonarQubeEnv('SonarQubeserver') {
-bat script: """
-sonar-scanner -D"sonar.projectKey=python" \
--D"sonar.sources=." \
--D"sonar.host.url=http://localhost:9000" \
--D"sonar.login=sqp_c84a56847a2f17688bfe80a1d533feac2acab633"
-"""
-}
-}
-}
-}
-}
-
-
